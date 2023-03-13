b43ba3f14768a129da10aa83cd1db1ef5fb0cab7
danieljuma5 <jumadaniel275@gmail.com>
2023-03-13 17:14:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// Declaring the currentUser
+const currentUser = 'Grace Hopper';
+
+//Declaring welcomeMessage and interpolating currentUser
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+//Outputting welcome message to upperCase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//Declaring shortGreeting to take initial letter of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
